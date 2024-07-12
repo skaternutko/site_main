7eaf614c0934e9201342c9ace0e099ec70068cad
Liam Thornton <skaternutko@gmail.com>
2024-07-12 13:46:42 -0400

    finished projects

--- common_scripts.js
+++ common_scripts.js
@@ -1,4 +1,4 @@
-const logo_array = [
+/*const logo_array = [
 'art/LOGOS/cannons_logo_creative.png',
 'art/LOGOS/cannons_logo_draws.png',
 'art/LOGOS/cannons_logo_games.png',
@@ -14,7 +14,22 @@ logo.addEventListener("click", changeLogo);
 
 function changeLogo(){
     logo.style.backgroundColor = "white";
-}
+}*/
 //logo changes to a different random logo on time interval or click
 //if click, reset the timer
 
+/*const moreText = document.querySelector("#more_text");
+const button = document.querySelector("#read-more");
+
+button.addEventListener("click", readMore);
+
+function readMore() {
+    if (moreText.style.visibility == "hidden") {
+    moreText.style.visibility = "visible";
+    button.innerHTML = "Show more";
+  } else {
+    moreText.style.visibility = "hidden";
+    button.innerHTML = "Show less";
+  }
+}
+*/
